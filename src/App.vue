6f7e8191472449286d1348772af1175b44c0e457
Emmanuel Oyawoye <89033579+yggd-millenia@users.noreply.github.com>
2026-03-08 21:08:54 -0500

    Event handling

--- src/App.vue
+++ src/App.vue
@@ -1,83 +1,100 @@
 <template>
-  <div class="card">
-    <h2>Message: {{ message }}</h2>
-    <h2>Number: {{ number }}</h2>
-    <button @click="changeMessageToUpperCase">
-      Change message to upper case
-    </button>
-    <button @click="incrementNumber">Increment number</button>
+  <h1>{{ message }}</h1>
+
+  <!-- replaceText is the name of a function defined in the script -->
+  <button v-on:click="replaceText('v-on is fun!')">Replace text</button>
+
+  <!-- v-on:click shortens to @click -->
+  <button @click="replaceText('v-on is fun!')">Replace text</button>
+
+  <hr />
+
+  <!-- using $event special variable to access native DOM event -->
+  <p>An input field where the user can ONLY enter numbers:</p>
+  <input type="text" @keydown="handleInput($event)" />
+
+  <hr />
+  <p>A demo of preventing the default behavior of an event.</p>
+  <p>Right click in the cyan box below will not show the context menu:</p>
+  <div
+    style="width: 100px; height: 100px; background-color: aqua"
+    @contextmenu.prevent="console.log('Show a custom context menu instead.')"
+  ></div>
+
+  <hr />
+
+  <p>A demo of stopping event propagation:</p>
+
+  <div id="mouseover" @mouseover="fun1">
+    <textarea @mouseover.stop="fun2($event)">This is a text area.</textarea>
   </div>
+  <!-- Since textarea is nested within div, mouseover textarea also means mouseover the parent div. -->
 
-  <div class="card">
-    <h2>Name: {{ wizard.name }}</h2>
-    <h2>Wand: {{ wizard.wand }}</h2>
-    <button @click="changeNameToUpperCase">Change name to upper case</button>
-    <button @click="changeWandCore">Change wand core</button>
-    <button @click="changeWizard">Change wizard</button>
-
-    <!-- You can also mutate a ref directly in event handlers -->
-    <button @click="wizard.wand.core = 'Unicorn hair'">Change wand core</button>
+  <hr />
+  <div>
+    Press down the "Enter" key will trigger a console log print:
+    <input
+      type="text"
+      @keydown.enter="console.log('You pressed the Enter key.')"
+    />
   </div>
-
-  <div class="card">
-    <h2>Array: {{ wizards }}</h2>
-    <button @click="wizards.push('Draco')">Add a new wizard</button>
+  <div>
+    Press down the "Arrow Down" key will trigger a console log print:
+    <input
+      type="text"
+      @keydown.down="console.log('You pressed the Arrow Down key.')"
+    />
+  </div>
+  <div>
+    Press down the "Space" key will trigger a console log print:
+    <input
+      type="text"
+      @keydown.space="console.log('You pressed the Space key.')"
+    />
+  </div>
+  <div>
+    Press down the "b" key will trigger a console log print:
+    <input type="text" @keydown.b="console.log('You pressed the B key.')" />
+  </div>
+  <div>
+    Press down the "Ctrl c" will trigger a console log print:
+    <input type="text" @keydown.ctrl.c="console.log('You pressed Ctrl c.')" />
   </div>
 </template>
 
 <script setup>
 import { ref } from 'vue'
 
-let message = ref('Hello, Reactivity!')
-let number = ref(1)
+let message = ref('Hello, v-on!')
 
-function changeMessageToUpperCase() {
-  message.value = message.value.toUpperCase()
-  console.log(message.value)
-}
-function incrementNumber() {
-  number.value += 1
-  console.log(number.value)
+function replaceText(msg) {
+  message.value = msg
 }
 
-let wizard = ref({
-  id: 1001,
-  name: 'Harry Potter',
-  house: 'Gryffindor',
-  age: 17, // Age during the final battle of Hogwarts
-  wand: {
-    core: 'Phoenix feather',
-    wood: 'Holly'
-  }
-})
-
-function changeNameToUpperCase() {
-  wizard.value.name = wizard.value.name.toUpperCase()
-}
-function changeWandCore() {
-  wizard.value.wand.core = 'Unicorn hair'
-}
-function changeWizard() {
-  wizard.value = {
-    id: 1002,
-    name: 'Hermione Granger',
-    house: 'Gryffindor',
-    age: 17,
-    wand: {
-      core: 'Dragon heartstring',
-      wood: 'Vine'
-    }
+function handleInput(event) {
+  // get the key code of the key pressed by user
+  let keyCode = event.keyCode
+  // if the key pressed is not a number key (0 - 9), don't show in the input box
+  if (keyCode < 48 || keyCode > 57) {
+    // vanilla JS code:
+    event.preventDefault()
   }
 }
 
-let wizards = ref(['Harry', 'Hermione', 'Ron'])
+function fun1() {
+  console.log('mouse over div')
+}
+
+function fun2(event) {
+  console.log('mouse over textarea')
+}
 </script>
 
 <style scoped>
-.card {
+#mouseover {
+  text-align: right;
   background-color: purple;
-  color: white;
-  padding: 20px 10px;
-  margin-bottom: 10px;
+  width: 300px;
+  height: 300px;
 }
 </style>
