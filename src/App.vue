aeea0ec1a933a32b7aa5a03998cd5e9b7be5c411
Emmanuel Oyawoye <89033579+yggd-millenia@users.noreply.github.com>
2026-03-08 21:00:03 -0500

    attribute binding

--- src/App.vue
+++ src/App.vue
@@ -1,102 +1,66 @@
 <template>
-  <!--The most basic form of data binding is text interpolation using the "Mustache" syntax (double curly braces)-->
-  <h1>message: {{ message }}</h1>
+  <h1>{{ message }}</h1>
+  <img v-bind:src="imageUrl" alt="" />
+  <br />
+
+  <!-- shorthand for v-bind -->
+  <img :src="imageUrl" alt="" />
+  <br />
+
+  <button @click="changeImg">Change image</button>
+
+  <br />
 
   <hr />
 
-  <!--{{message}} refers to the message we defined in data-->
-  <h1>number: {{ number }}</h1>
-
-  <h1>doubleNum(50): {{ doubleNum(50) }}</h1>
-
-  <h1>number * 2 = {{ number * 2 }}</h1>
-
-  <h1>
-    {{ number > 150 ? 'number is great than 150' : 'number is less than 150' }}
-  </h1>
-
-  <h1 v-text="number"></h1>
+  <input type="text" :value="defaultInputText" />
 
   <hr />
 
-  <h1>harry: {{ harry }}</h1>
+  <p :class="className">Harry Potter</p>
 
-  <h1>harry.name: {{ harry.name }}</h1>
+  <!-- define a JS object in :class -->
+  <p :class="{ inactive: isInactive, center: isCenter }">
+    
+    Harry Potter
+  </p>
 
-  <h1>hogwartsWizards: {{ hogwartsWizards }}</h1>
-
-  <h1>hogwartsWizards[0]: {{ hogwartsWizards[0] }}</h1>
-  <hr />
-
-  <h1>rawHtml: {{ rawHtml }}</h1>
-
-  <h1 v-text="rawHtml"></h1>
-
-  <h1 v-html="rawHtml"></h1>
+  <!-- define a JS array in :class -->
+  <p :class="['active', 'center']">Harry Potter</p>
 </template>
 
 <script setup>
-let message = 'Hello, Vue!'
-let number = 50
+import { ref } from 'vue'
 
-function doubleNum(num) {
-  return num * 2
+let message = 'Hello, v-bind!'
+let imageUrl = ref('public/img/banner_1.jpg')
+
+function changeImg() {
+  imageUrl.value = 'public/img/banner_2.jpg'
 }
 
-let harry = {
-  id: 1001,
-  name: 'Harry Potter',
-  house: 'Gryffindor',
-  age: 17, // Age during the final battle of Hogwarts
-  wand: {
-    core: 'Phoenix feather',
-    wood: 'Holly'
-  }
-}
-const hogwartsWizards = [
-  {
-    id: 1001,
-    name: 'Harry Potter',
-    house: 'Gryffindor',
-    age: 17,
-    wand: {
-      core: 'Phoenix feather',
-      wood: 'Holly'
-    }
-  },
-  {
-    id: 1002,
-    name: 'Hermione Granger',
-    house: 'Gryffindor',
-    age: 17,
-    wand: {
-      core: 'Dragon heartstring',
-      wood: 'Vine'
-    }
-  },
-  {
-    id: 1003,
-    name: 'Ron Weasley',
-    house: 'Gryffindor',
-    age: 17,
-    wand: {
-      core: 'Unicorn hair',
-      wood: 'Willow'
-    }
-  },
-  {
-    id: 1004,
-    name: 'Draco Malfoy',
-    house: 'Slytherin',
-    age: 17,
-    wand: {
-      core: 'Dragon heartstring',
-      wood: 'Hawthorn'
-    }
-  }
-]
+let defaultInputText = 'Write something here...'
 
-let rawHtml = '<span style="color: red">This should be red.</span>'
+let className = ref('active')
+let isInactive = ref(true)
+let isCenter = ref(false)
 </script>
 
-<style lang="scss" scoped></style>
+<style scoped>
+img {
+  max-width: 300px;
+}
+
+.active {
+  color: green;
+}
+
+.inactive {
+  color: red;
+  text-decoration: line-through;
+}
+
+.center {
+  text-align: center;
+}
+</style>
