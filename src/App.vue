418cfcee8dfc8fdc47f7a52d7e65fa5232c0f129
Emmanuel Oyawoye <89033579+yggd-millenia@users.noreply.github.com>
2026-03-08 21:19:44 -0500

    List Rendering

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,62 @@
 <template>
-  <div>Hello, Emmanuel Welcome to Vue!</div>
+  <h1>{{ message }}</h1>
+  <button @click="sortUsersByAge">Sort users by age</button>
+  <ul>
+    <li v-for="(user, index) in users" :key="user.id">
+      {{ index }} - {{ user.id }} - {{ user.name }} - {{ user.age }} -
+      {{ user.isActive }}
+    </li>
+  </ul>
+
+  <hr />
+
+  <table>
+    <tr>
+      <th>Index</th>
+      <th>Id</th>
+      <th>Name</th>
+      <th>Age</th>
+      <th>Operation</th>
+    </tr>
+    <tr v-for="(user, index) in users" :key="user.id">
+      <td>{{ index }}</td>
+      <td>{{ user.id }}</td>
+      <td :class="{ inactive: !user.isActive }">{{ user.name }}</td>
+      <td>{{ user.age }}</td>
+      <td>
+        <button @click="user.isActive = !user.isActive">
+          {{ user.isActive ? 'Deactivate' : 'Restore' }}
+        </button>
+      </td>
+    </tr>
+  </table>
+
+  <hr />
+  <p>Use v-for to iterate through the properties of an object:</p>
+  <div v-for="(value, key, index) in users[0]">
+    {{ index }} - {{ key }}: {{ value }}
+  </div>
 </template>
 
 <script setup>
+import { ref } from 'vue'
 
+let message = ref('Hello, v-for!')
+
+const users = ref([
+  { id: 1001, name: 'John Smith', age: 26, isActive: false },
+  { id: 1002, name: 'Tom Doe', age: 16, isActive: false },
+  { id: 1003, name: 'Frankin Wong', age: 18, isActive: true }
+])
+
+function sortUsersByAge() {
+  users.value.sort((a, b) => a.age - b.age)
+}
 </script>
 
-<style lang="scss" scoped>
-
+<style scoped>
+.inactive {
+  color: red;
+  text-decoration: line-through;
+}
 </style>
