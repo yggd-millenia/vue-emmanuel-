371fe2a7442cdae7e35510557149c7e309e15367
Emmanuel Oyawoye <89033579+yggd-millenia@users.noreply.github.com>
2026-03-13 19:05:21 -0500

    introduction to vue components

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,21 @@
 <template>
-  <div>Hello, Emmanuel Welcome to Vue!</div>
+  <ButtonCounter></ButtonCounter>
+  <ButtonCounter></ButtonCounter>
+
+  <button-counter></button-counter>
+
+  <hr />
+
+  <div style="background-color: orange; padding: 5px">
+    This is the root component.
+    <ComponentA></ComponentA>
+    <ComponentA></ComponentA>
+  </div>
 </template>
 
 <script setup>
-
+import ButtonCounter from './ButtonCounter.vue'
+import ComponentA from '@/ComponentA.vue'
 </script>
 
-<style lang="scss" scoped>
-
-</style>
+<style scoped></style>
