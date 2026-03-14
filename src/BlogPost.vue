8263f23be68290effbb30dc139fe86297d25f424
Emmanuel Oyawoye <89033579+yggd-millenia@users.noreply.github.com>
2026-03-13 19:24:24 -0500

    Component v-model for a two-way data binding

---
+++ src/BlogPost.vue
@@ -0,0 +1,45 @@
+<template>
+  <div class="blog-post">
+    <p>
+      {{ message }}
+    </p>
+    <h2>{{ id }} - {{ blogPostTitle }}</h2>
+    <div>Modify the title: 
+      <input type="text" v-model="blogPostTitle" />
+    </div>
+    <h4>{{ blogPostContent }}</h4>
+    <div>
+      Modify the content:
+      <input type="text" v-model="blogPostContent" />
+    </div>
+    <button @click="$emit('delete-blog-post', id)">Delete post</button>
+    <button @click="emitDeletePostEvent(id)">Delete post</button>
+  </div>
+</template>
+
+<script setup>
+import { ref } from 'vue'
+
+let message = ref('This is the BlogPost component.')
+defineProps(['id'])
+
+const emit = defineEmits(['delete-blog-post'])
+function emitDeletePostEvent(id) {
+  emit('delete-blog-post', id)
+}
+
+let blogPostTitle = defineModel('blogPostTitle') // blogPostTitle is a ref object
+let blogPostContent = defineModel('blogPostContent') // blogPostContent is a ref object
+</script>
+
+<style scoped>
+.blog-post {
+  background-color: aqua;
+  padding: 10px;
+  margin-bottom: 10px;
+}
+
+input {
+  width: 100%;
+}
+</style>
